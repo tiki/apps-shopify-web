ce0807de5ff3ed7a42f7f941de59c5fde6c9293e
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-09-01 10:48:57 -0300

    fix: discount save endpoint

--- src/pages/discount/product/Create.tsx
+++ src/pages/discount/product/Create.tsx
@@ -116,7 +116,7 @@ export function DiscountProductCreate() {
       },
     };
     await authenticatedFetch(
-      'https://tiki.shopify.brgweb.com.br/api/latest/discount',
+      'https://intg-shpfy.pages.dev/api/latest/discount',
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
