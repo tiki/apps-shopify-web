07e77be472d402980b20ddaeb0dfd74e381535af
Miro Benicio <mirodantas27@gmail.com>
2023-09-15 14:34:17 -0300

    fix: catch error in the create discount method

--- src/pages/discount/order/Create.tsx
+++ src/pages/discount/order/Create.tsx
@@ -92,7 +92,7 @@ export function DiscountOrderCreate() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
-    });
+    }).catch(error => console.log(error));
     redirect.dispatch(Redirect.Action.ADMIN_SECTION, {
       name: Redirect.ResourceType.Discount,
     });
--- src/pages/discount/product/Create.tsx
+++ src/pages/discount/product/Create.tsx
@@ -121,7 +121,7 @@ export function DiscountProductCreate() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
-    });
+    }).catch(error => console.log(error));
     redirect.dispatch(Redirect.Action.ADMIN_SECTION, {
       name: Redirect.ResourceType.Discount,
     });
