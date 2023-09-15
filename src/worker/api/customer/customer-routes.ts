d91c843269e7dcb9541cd28e3063f15dabbc78bc
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-09-15 07:36:14 -0300

    fix: add console logs to benchmark

--- src/worker/api/customer/customer-routes.ts
+++ src/worker/api/customer/customer-routes.ts
@@ -22,21 +22,42 @@ export async function redact(): Promise<Response> {
 }
 
 export async function discount(request: IRequest, env: Env): Promise<Response> {
+  let previous = new Date().getTime();
   const token = request.headers.get(API.Consts.AUTHORIZATION) as string;
+  console.log('token', new Date().getTime() - previous);
+  previous = new Date().getTime();
   const address = request.headers.get('X-Tiki-Address') as string;
+  console.log('address', new Date().getTime() - previous);
+  previous = new Date().getTime();
   const signature = request.headers.get('X-Tiki-Signature') as string;
+  console.log('signature', new Date().getTime() - previous);
+  previous = new Date().getTime();
 
   Throw.ifNull(token, 'Authorization');
   Throw.ifNull(address, 'X-Tiki-Address');
   Throw.ifNull(signature, 'X-Tiki-Signature');
 
   const tiki = new Tiki(env);
+  console.log('tiki', new Date().getTime() - previous);
+  previous = new Date().getTime();
   const claims = await tiki.verifyEcdsa(token.replace('Bearer ', ''));
+  console.log('claims', new Date().getTime() - previous);
+  previous = new Date().getTime();
   const appId = claims.get('sub') as string;
+  console.log('appId', new Date().getTime() - previous);
+  previous = new Date().getTime();
   const body = await request.text();
+  console.log('body', new Date().getTime() - previous);
+  previous = new Date().getTime();
   const json: CustomerDiscount = JSON.parse(body);
+  console.log('json', new Date().getTime() - previous);
+  previous = new Date().getTime();
   const adminToken = await tiki.admin();
+  console.log('adminToken', new Date().getTime() - previous);
+  previous = new Date().getTime();
   const registry = await tiki.registry(adminToken, address, appId);
+  console.log('registry', new Date().getTime() - previous);
+  previous = new Date().getTime();
   if (Number(registry.id) !== Number(json.customerId)) {
     throw new API.ErrorBuilder().message('Invalid X-Tiki-Address').error(403);
   }
