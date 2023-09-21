33871f0892807e3bae1ea5639033b08d6b3681b3
Miro Benicio <mirodantas27@gmail.com>
2023-09-21 17:11:14 -0300

    Fix: Handle Error in form submit

--- src/pages/discount/product/Create.tsx
+++ src/pages/discount/product/Create.tsx
@@ -123,9 +123,11 @@ export function DiscountProductCreate() {
   const submit = async () => {
     const imageId = await handleBannerFile().catch(error=>{
       console.log(error)
-      return setSubmitError("Ops, something went wrong during the image upload, try another one.")
     })
-    if(!title || !minValue) return setSubmitError("Title and Discount Value are required")
+    
+    if(imageId === undefined) return setSubmitError("Ops, something went wrong during the image upload, try another one.")
+
+    if(title === undefined || minValue === undefined) return setSubmitError("Title and Discount Value are required")
 
     const body: DiscountReq = {
       title: title ?? '',
